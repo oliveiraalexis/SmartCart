47fa0cbba17312e0820e809ad646cbf089082597
RADIXENGRJ\alexis.oliveira <alexis.oliveira@radixeng.com.br>
2023-09-07 12:23:57 -0300

    creating basic react native structure

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  }
+});
+
+export default App;
