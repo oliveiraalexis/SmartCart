eb1b4c8c886d14291cedec5797b9a7f7080d7d00
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2023-12-19 19:45:20 -0300

    Improving CardList component

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -8,7 +8,7 @@ export const CardList = () => {
         <TouchableHighlight style={styles.container}>
             <View style={styles.view}>
                 <Text style={styles.text}>Lista de compras de AGO/2023</Text>
-                <Button iconName='trash' bRadius={5} bBackgroundColor='#8F0000'/>
+                <Button iconName='trash' bRadius={5} bBackgroundColor='#161E33'/>
             </View>
         </TouchableHighlight>
     )
@@ -17,11 +17,10 @@ export const CardList = () => {
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        height: 100,
+        height: 60,
         backgroundColor: '#161E33',
         borderRadius: 10,
-        marginTop: 20,
-        padding: 15
+        marginTop: 20
     },
     text: {
         color: '#FFFFFF',
@@ -33,7 +32,8 @@ const styles = StyleSheet.create({
     view: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'flex-end',
+        alignItems: 'center',
+        marginHorizontal: 10,
         flex: 1
     }
 })
