aa66e57a725929f71eacd7d0845e7af3b77e4c41
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2023-12-21 18:59:17 -0300

    Refactoring Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,91 +1,45 @@
 import React, {useState} from 'react'
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Platform } from 'react-native'
 import CheckBox from '@react-native-community/checkbox'
-import DropDownPicker from 'react-native-dropdown-picker';
 import { Button } from '../Button/Button';
 
 export const Product = () => {
 
     const [isSelected, setSelection] = useState(false);
-    const [qtde, setQtde] = useState(1);
-    const [preco, setPreco] = useState(0.00);
+    const checkColor = Platform.OS === 'android' 
+        ? {true: '#FFFFFF', false: '#FFFFFF'} 
+        : {
+            tintColor: '#FFFFFF',
+            onCheckColor: '#FFFFFF',
+            onFillColor: '#FFFFFF',
+            onTintColor: '#FFFFFF'
+        }
 
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState('un');
-    const [items, setItems] = useState([
-        {label: 'Kg', value: 'kg'},
-        {label: 'Un', value: 'un'}
-    ]);
+        console.log(checkColor)
 
     return (
-        <View style={styles.container}> 
-            <View style={styles.viewup}>
-                <View style={styles.viewcheck}>
+        <View style={styles.container}>
+            <View style={styles.viewtop}>
+                <View style={styles.viewleft}>                
                     <CheckBox
                         disabled={false}
                         value={isSelected}
                         onValueChange={() => setSelection(!isSelected)}
+                        tintColors={checkColor}
+                        //IOS colors:
+                        // tintColor= '#FFFFFF'
+                        // onCheckColor= '#FFFFFF'
+                        // onFillColor= '#FFFFFF'
+                        // onTintColor= '#FFFFFF'
                     />
                     <Text style={styles.title}>Torrada Integral Adria</Text>
                 </View>
-                <Button/>
-            </View>
-            <View style={styles.viewdown}>
-                <View style={styles.viewdownelements}>
-                    <Text style={styles.text}>Tipo</Text>
-                    <View>
-                        <DropDownPicker
-                            style={styles.dropdown}
-                            dropDownContainerStyle={{
-                                backgroundColor: '#D9D9D9',
-                                maxHeight: 60,
-                                maxWidth: 80,
-                                borderColor: '#D9D9D9',
-                                fontSize: 18,
-                                fontWeight: 'bold'
-                            }}
-                            labelStyle={{
-                                fontSize: 18,
-                                fontWeight: 'bold'
-                            }}
-                            open={open}
-                            value={value}
-                            items={items}
-                            setOpen={setOpen}
-                            setValue={setValue}
-                            setItems={setItems}
-
-                            multiple={false}
-                        />
-                    </View>
-                </View>
-                <View style={styles.viewdownelements}>
-                    <Text style={styles.text}>Qtde</Text>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setQtde}
-                        value={qtde}
-                        defaultValue='1'
-                        keyboardType="numeric"
-                    />
-                </View>
-                <View style={styles.viewdownelements}>
-                    <Text style={styles.text}>Preço unit.</Text>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setPreco}
-                        value={preco}
-                        defaultValue="0,00"
-                        keyboardType="numeric"
-                    />
-                </View>
-                <View style={styles.viewdownelements}>
-                    <Text style={styles.text}>Total</Text>
-                    <View style={styles.price}>
-                        <Text style={styles.title}>R$ 100,00</Text>
-                    </View>
+                <View style={styles.viewright}>
+                    <Button  iconName='pencil' bBackgroundColor='#161E33' />
                 </View>
             </View>
+            <Text style={styles.text}>Quantidade: 1un</Text>
+            <Text style={styles.text}>Preço Total: R$ 9,99</Text>
         </View>
     )
 }
@@ -95,58 +49,34 @@ const styles = StyleSheet.create({
         width: '100%',
         borderBottomColor: '#717785',
         borderBottomWidth: 2,
-        paddingVertical: 15
+        paddingVertical: 15,
+        paddingHorizontal: 5
     },
     title: {
         color: '#ffffff',
         fontWeight: 'bold',
-        fontSize: 20,
+        fontSize: 15,
     },
     text: {
         color: '#717785',
         fontWeight: 'bold',
-        fontSize: 18
-    },
-    viewup: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start'
+        fontSize: 12,
+        marginTop: 6,
+        marginLeft: 7
 
     },
-    viewdown: {
+    viewtop: {
         flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginVertical: 10
-    },
-    viewdownelements: {
-        alignItems: 'center',
-        flex: 1
-    },
-    viewcheck: {
+        justifyContent: 'space-between'
+    }
+    ,
+    viewleft: {
         flexDirection: 'row',
+        justifyContent: 'flex-start',
         alignItems: 'center'
+
     },
-    input: {
-        height: 35,
-        width: '70%',
-        marginTop: 12,
-        borderRadius: 10,
-        paddingVertical: 5,
-        paddingStart: 10,
-        backgroundColor: '#D9D9D9',
-        fontSize: 20,
-        fontWeight: 'bold'
-    },
-    dropdown: {
-        marginTop: 12,
-        borderWidth: 0,
-        borderRadius: 10,
-        backgroundColor: '#D9D9D9',
-        width: 80,
-        minHeight: 35,
-        maxMenuHeight: 10
-    },
-    price: {
-        marginTop: 17,
+    viewright: {
+        marginTop: 7
     }
 })
