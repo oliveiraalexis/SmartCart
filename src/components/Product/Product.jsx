608fcfaab0c00b652a89a0f16023ebd3c0455f15
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2024-02-28 20:37:25 -0300

    Adding delete button in Product component

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -13,17 +13,18 @@ export const Product = ({nome, tipo, qtde, preco, toggleProductForm}) => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.viewtop}>
                 <View style={styles.viewleft}>                
-                    <Checkbox value={isSelected} onValueChange={onValueChange}/>
-                    <Text style={styles.title}>{nome}</Text>
+                    <View style={styles.viewtop}>
+                        <Checkbox value={isSelected} onValueChange={onValueChange}/>
+                        <Text style={styles.title}>{nome}</Text>
+                    </View>
+                    <Text style={styles.text}>Quantidade: {qtde} {tipo}</Text>
+                    <Text style={styles.text}>Preço Total: {parseFloat(qtde) * parseFloat(preco)}</Text>
                 </View>
                 <View style={styles.viewright}>
-                    <Button onPress={toggleProductForm} iconName='pencil' bBackgroundColor='#161E33' />
+                    <Button onPress={null} iconName='trash' bBackgroundColor='#161E33' size={15}/>
+                    <Button onPress={toggleProductForm} iconName='pencil' bBackgroundColor='#161E33' size={15}/>
                 </View>
-            </View>
-            <Text style={styles.text}>Quantidade: {qtde} {tipo}</Text>
-            <Text style={styles.text}>Preço Total: {parseFloat(qtde) * parseFloat(preco)}</Text>
         </View>
     )
 }
@@ -34,7 +35,9 @@ const styles = StyleSheet.create({
         borderBottomColor: '#717785',
         borderBottomWidth: 2,
         paddingVertical: 15,
-        paddingHorizontal: 5
+        paddingHorizontal: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     title: {
         color: '#ffffff',
@@ -47,20 +50,16 @@ const styles = StyleSheet.create({
         fontSize: 12,
         marginTop: 6,
         marginLeft: 7
-
     },
     viewtop: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
-    }
-    ,
-    viewleft: {
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
         alignItems: 'center'
-
+    },
+    viewleft: {
+        justifyContent: 'space-around'
     },
     viewright: {
-        marginTop: 7
+        marginTop: 7,
+        justifyContent: 'space-between'
     }
 })
