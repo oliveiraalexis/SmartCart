2c80dae07d98b367fbf4f346c89ce3f916fbdf29
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2024-03-15 08:40:12 -0300

    Adding FlatList component to the ListsScreen

--- src/pages/ListsScreen/ListsScreen.jsx
+++ src/pages/ListsScreen/ListsScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useRef, useState } from 'react'
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
+import { Text, View, StyleSheet, SafeAreaView, FlatList } from 'react-native'
 import { Header } from '../../components/Header/Header'
 import { Button } from '../../components/Button/Button'
 import { CardList } from '../../components/CardList/CardList'
@@ -51,21 +51,37 @@ export const ListsScreen = ({navigation}) => {
         setShopLists(listValues)
     }
 
+    const listFooterComponent = (
+        <View style={{marginTop: 20}}>
+            <Button onPress={() => toggleListForm()} margin iconName='plus' bRadius={30} bBackgroundColor='#161E33' width={50} height={50}/>
+        </View>
+    )
+
+    const emptyListComponent = (
+        <Text style={styles.text}>Nenhuma lista criada. {'\n'}Crie uma lista utilizando {'\n'}o botão “+” abaixo.</Text>
+    )
+
+    const flatListRef = React.useRef()
+
+    function handleScrollToEnd() {
+        if (flatListRef.current) {
+            flatListRef.current.scrollToEnd()
+        }
+    }
+
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
             <SafeAreaView style={styles.container}>
                 <Header title='SMARTCART'/>
-                <View style={styles.body}>
-                    {shopLists.length > 0 && (shopLists.map((title, index)=>{
-                        return <CardList title={title} onPress={navigationToDetailScreen} deleteList={deleteList} toggleListForm={() => toggleListForm(title)} key={index} />
-                    }))}
-                    { shopLists.length == 0 && (<Text style={styles.text}>
-                        Nenhuma lista criada. {'\n'}Crie uma lista utilizando {'\n'}o botão “+” abaixo.
-                    </Text>)}
-                    <View style={{marginTop: 20}}>
-                        <Button onPress={() => toggleListForm()} margin iconName='plus' bRadius={30} bBackgroundColor='#161E33' width={50} height={50}/>
-                    </View>
-                </View>
+                <FlatList
+                    ref={flatListRef}
+                    onContentSizeChange={handleScrollToEnd} 
+                    contentContainerStyle={styles.body}
+                    data={shopLists}
+                    renderItem={({item}) => <CardList title={item} onPress={navigationToDetailScreen} deleteList={deleteList} toggleListForm={() => toggleListForm(item)}/>}
+                    ListEmptyComponent={emptyListComponent}
+                    ListFooterComponent={listFooterComponent}
+                />
                 {ListFormVisible && (<BottomSheet
                     ref={bottomSheetRef}
                     index={0}
@@ -82,13 +98,12 @@ export const ListsScreen = ({navigation}) => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#0A0E17'
         
     },
     body: {
-        backgroundColor: '#0A0E17',
         alignItems: 'center',
-        flex: 1,
         padding: 20
     },
     text: {
