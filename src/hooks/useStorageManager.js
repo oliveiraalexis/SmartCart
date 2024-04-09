d5f14c7b636d964d8b7502b4e175ed0b014c75c6
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2024-04-09 17:55:40 -0300

    Using the useDuplicatedItemAlert hook in the useStorageManager hook

--- src/hooks/useStorageManager.js
+++ src/hooks/useStorageManager.js
@@ -1,4 +1,5 @@
 import { search, save, remove } from "../services/Storage"
+import { useDuplicatedItemAlert } from "./useDuplicatedItemAlert"
 
 export const deleteItem = (storageKey, item, refreshItemArray) => {
     const storage = search(storageKey)
@@ -9,22 +10,42 @@ export const deleteItem = (storageKey, item, refreshItemArray) => {
 }
 
 export const addItem = (storageKey, item, toggleForm) => {
-    const storage = search(storageKey) 
-    let newStorage = [...storage]
-    newStorage.push(item)
-    save(storageKey, newStorage)
-    toggleForm()
+
+    const index = findIndex(storageKey, item)
+    if (index < 0){
+
+        const storage = search(storageKey) 
+        let newStorage = [...storage]
+        newStorage.push(item)
+        save(storageKey, newStorage)
+        toggleForm()
+    } else {
+        useDuplicatedItemAlert(storageKey)
+    }
 }
 
 export const editItem = (storageKey, item, newItem, toggleForm) => {
+    
+    const indexNewItem = findIndex(storageKey, newItem)
+    
+    if (indexNewItem < 0) {
+        const storage = search(storageKey)
+        const indexItem = findIndex(storageKey, item)
+        let newStorage = [...storage]
+    
+        newStorage[indexItem] = newItem
+        save(storageKey, newStorage)
+        if(toggleForm) toggleForm()
+    } else {
+        useDuplicatedItemAlert(storageKey)
+    }
+}
+
+const findIndex = (storageKey, item) => {
     const storage = search(storageKey)
-    let newStorage = [...storage]
     let index = -1
     
-    if (storageKey === 'Lists') index = newStorage.indexOf(item)
-    else index = newStorage.findIndex(value => JSON.stringify(value) === JSON.stringify(item))
-
-    newStorage[index] = newItem
-    save(storageKey, newStorage)
-    if(toggleForm) toggleForm()
+    if (storageKey === 'Lists') index = storage.indexOf(item)
+    else index = storage.findIndex(value => JSON.stringify(value) === JSON.stringify(item))
+    return index
 }
