0d7d3b22cee0ac8a3b8256cea6c6d6ef33a4d51d
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2024-04-13 22:10:40 -0300

    Altering NavigationContainer to hide BootSplash

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,13 +4,18 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { ListsScreen } from '../pages/ListsScreen/ListsScreen'
 import { ListDetailScreen } from '../pages/ListDetailScreen/ListDetailScreen'
+import BootSplash from "react-native-bootsplash";
 
 export const Routes = () => {
 
     const Stack = createNativeStackNavigator()
 
     return (
-        <NavigationContainer>
+        <NavigationContainer 
+            onReady={() => {
+                BootSplash.hide({fade: true});
+            }}
+        >
             <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='ListsScreen'>
                 <Stack.Screen name='ListsScreen' component={ListsScreen}/>
                 <Stack.Screen name='ListDetailScreen' component={ListDetailScreen}/>
