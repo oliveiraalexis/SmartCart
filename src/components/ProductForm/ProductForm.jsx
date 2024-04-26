045d8b1cc7d18f4f3f15cda41f08be50050a3e57
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2024-04-26 20:07:43 -0300

    Adding marginBottom

--- src/components/ProductForm/ProductForm.jsx
+++ src/components/ProductForm/ProductForm.jsx
@@ -160,6 +160,7 @@ const styles = StyleSheet.create({
     button:{
         flexDirection: 'row',
         justifyContent: 'space-evenly',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginBottom: 20
     }
 })
