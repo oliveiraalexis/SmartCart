178b8f5d6625e0dec0480c7ac1103c87f87a1ea2
oliveiraalexis <alexis.silvaoliveira@gmail.com>
2024-04-26 20:07:29 -0300

    Adding marginBottom

--- src/components/ListForm/ListForm.jsx
+++ src/components/ListForm/ListForm.jsx
@@ -56,6 +56,7 @@ const styles = StyleSheet.create({
     button:{
         flexDirection: 'row',
         justifyContent: 'space-evenly',
-        alignItems: 'center'
+        alignItems: 'center',
+        marginBottom: 20
     }
 })
